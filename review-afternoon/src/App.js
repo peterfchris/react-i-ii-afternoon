46aefefc40be2aca08ff5e5e50858c6dee70c987
Peter Forsgren Christensen <peter.f.chris@gmail.com>
2019-05-01 16:02:25 -0600

    Initial styling

--- review-afternoon/src/App.js
+++ review-afternoon/src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header className="header">
+      <h3>Home</h3>
+      </header>
+      <div className="white-container">
+
+      </div>
+      <div className="buttons">
+        <button className="previous">{'< Previous'}</button>
+        <button className="next">{'Next >'}</button>
+      </div>
     </div>
   );
 }
