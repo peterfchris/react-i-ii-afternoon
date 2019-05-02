ed00e380cea20a30b43d1fb1745e165be2bccc40
Peter Forsgren Christensen <peter.f.chris@gmail.com>
2019-05-01 18:27:52 -0600

    afternoon project completed

--- review-afternoon/src/Components/Layout.js
+++ review-afternoon/src/Components/Layout.js
@@ -16,21 +16,23 @@ export default class Layout extends Component {
     render() {
        const user = this.props.user.map((user) => {
            return (
-               <div className="main-content">
-                   <h1>{user.name.first}{' '}{user.name.last}</h1>
-                   
-                    From: {user.city}{', '}{user.country}
-                    Job Title: {user.title}
-                    Employer: {user.employer}
-                    <br />
-                    Favorite Movies:
-                        <ol>
-                            <li>{user.favoriteMovies[0]}</li>
-                            <li>{user.favoriteMovies[1]}</li>
-                            <li>{user.favoriteMovies[2]}</li>
-                        </ol>
-                    
-                  
+               <div>
+                   <div className="count">
+                       <h2>{user.id}/25</h2>
+                   </div>
+                    <div className="main-content">
+                        <h1 className="user-name">{user.name.first}{' '}{user.name.last}</h1>
+                            <p><strong>From:</strong> {user.city}{', '}{user.country}</p>
+                            <p><strong>Job Title:</strong> {user.title}</p>
+                            <p><strong>Employer:</strong> {user.employer}</p>
+                            <br />
+                            <p><strong>Favorite Movies:</strong></p>
+                                <ol>
+                                    <li>{user.favoriteMovies[0]}</li>
+                                    <li>{user.favoriteMovies[1]}</li>
+                                    <li>{user.favoriteMovies[2]}</li>
+                                </ol>
+                    </div>
                </div>
            )
        })
